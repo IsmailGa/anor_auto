23b17af2bd25af9f30c8d74a4d0e624a0c7d1771
Ismail <gaismail777@gmail.com>
2024-12-15 17:12:52 +0500

    Deploy

--- src/pages/Home/homeComponents/Location/Location.js
+++ src/pages/Home/homeComponents/Location/Location.js
@@ -8,7 +8,7 @@ export const MapLocation = () => {
   return (
     <div className="location">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d187.52080743891037!2d69.2143966877239!3d41.236305508595095!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1730917636353!5m2!1sen!2s"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d750.0824254327558!2d69.2144793!3d41.2363756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae6100218aa5e1%3A0x7d0c0fa0d4ef399e!2sAnor%20Auto!5e0!3m2!1sen!2s!4v1734173078136!5m2!1sen!2s"
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
